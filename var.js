336f1b69e02652e9f8cfe3bf005e88b5e2d230a5
allbann76 <tuboltsevb@gmail.com>
2016-03-02 01:11:22 +0300

    test last 1:11 night =)

--- var.js
+++ var.js
@@ -1,3 +1,16 @@
+
+var fs = require("fs");
+
 function getRand(min,max) {
 	return ~~(Math.random()*(max-min)+min);
 }
+
+var append = function(){
+	fs.appendFile('dump.txt', getRand(1,9999)+"\n", 'utf8', function(err)
+	{
+	if(err) throw err;
+	console.log('test');
+	});
+}
+setInterval(append,1000);
+
