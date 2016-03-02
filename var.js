464ef67916af21eb76594e828924a1896bbd97e7
allbann76 <tuboltsevb@gmail.com>
2016-03-02 14:08:58 +0300

    fs read

--- var.js
+++ var.js
@@ -1,5 +1,16 @@
 
 var fs = require("fs");
+var http = require('http');
+
+var server = new http.Server();
+
+server.listen(5000, '127.0.0.1');
+var cont = 0;
+
+
+
+
+
 
 function getRand(min,max) {
 	return ~~(Math.random()*(max-min)+min);
@@ -10,7 +21,16 @@ var append = function(){
 	{
 	if(err) throw err;
 	console.log('test');
+	cont++;
 	});
 }
+server.on('request', function(req, res) {
+
+  //"в файл записано "+cont+"раз, объем файла - ");
+  fs.readFile("dump.txt", "utf8", function(error, text) {
+  if (error)throw error;
+  res.end(text);
+});
+});
 setInterval(append,1000);
 
